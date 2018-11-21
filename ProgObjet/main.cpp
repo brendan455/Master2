c25f2db5a9ee175d6235c40378c45d350a802202
CreatorOfMoon <43032499+CreatorOfMoon@users.noreply.github.com>
2018-11-21 09:20:52 +0100

    Create main.cpp

---
+++ ProgObjet/main.cpp
@@ -0,0 +1,17 @@
+#include main.hpp
+#include list.h
+#include machin.h
+
+
+
+void run(){
+  if true {
+  cout << "Done" ;
+  }
+}
+
+
+int main( int argc, const char* argv []){
+  run();
+  return 0;
+}
