81513c43eb66536c252509ad9244808752e25b98
CreatorOfMoon <43032499+CreatorOfMoon@users.noreply.github.com>
2018-11-21 09:57:57 +0100

    Update Cabinet.cpp

--- ProgObjet/Cabinet.cpp
+++ ProgObjet/Cabinet.cpp
@@ -1,29 +1,30 @@
 
-#pragma once
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <list>
+#include <string>
 #include medecin.h
 #include dossier.h
+
+
 using namespace std;
 
 class Cabinet
 {
 	private: // Attribut
-		char* nom;
-    char* adresse;
-    List(Medecin) medecins;
-    List(Dossier) dossiers,
+		string nom;
+   		string adresse;
+    		List(Medecin) medecins;
+    		List(Dossier) dossiers,
 
 	public: // Méthodes
-		// Constructeur
-	Cabinet(char*,char*);
+	// Constructeur
+		Cabinet(string,string);	
+	// Accesseurs
+		string getNomCabinet(){};
+		void setNewNomCabinet(string){};
 		
-		// Accesseurs
-		int getNomCabinet();
-		void setNewNomCabinet(char*);
-		
-		// Affichage
+	// Comportement
 		void afficherCabinet();
 }
