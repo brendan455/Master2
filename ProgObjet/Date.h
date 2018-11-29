ec1349c9bf22ff4c19eed2b9ed2a8e0fc3d348da
CreatorOfMoon <43032499+CreatorOfMoon@users.noreply.github.com>
2018-11-29 10:16:47 +0100

    Update Date.h

--- ProgObjet/Date.h
+++ ProgObjet/Date.h
@@ -5,18 +5,29 @@
 class Date
 {
 private: // Attribut
-    int month;
-    int day;
-    int year;
+	int month;
+	int day;
+	int year;
 
 public: // Méthodes
-	// Constructeur
-    Date(int month,int day,int year);
-
-    // Affichage
-    void Debut();
-    void Fin();
-    void display2();
-    void increment();
+// Constructeur
+    	Date(int month,int day,int year);
+	
+// Accesseur
+	void setDay(int);
+	int getDay();
+	void setMonth(int);
+	int getMonth();
+	void setYear(int);
+	int getYear();
+		
+// Affichage
+	// Affichage d'un date au format XX/XX/XXXX
+    	void affichage();
+	// Affichage d'un date au format XX/MOIS/XXXX
+	void affichage2();
+	// Affichage d'un date au format MOIS/XX/XXXX
+	void affichage3();
+	
 };
 #endif 
