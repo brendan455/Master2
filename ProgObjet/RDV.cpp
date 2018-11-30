8ce82ebaf356642d841da9b8730f7d7e73db4c79
CreatorOfMoon <43032499+CreatorOfMoon@users.noreply.github.com>
2018-11-30 10:57:53 +0100

    Update RDV.cpp

--- ProgObjet/RDV.cpp
+++ ProgObjet/RDV.cpp
@@ -7,70 +7,36 @@ using namespace std;
 
 
 // Code du contructeur de RDV
-RDV::RDV(vector<Medecin> nom_medecin, vector<Patient> nom_patient, vector<Date> dateRDV){
-	ajouterMedecin(nom_medecin);
-	ajouterPatient(nom_patient);
-	ajouterDate(dateRDV);
-}
-
-
-// Affichage d'un objet de type RDV
-void RDV::afficherRDVPatient(){
-	string nom_patient;
-	cout << "	- Le rendez-vous du patient : "<< this->nom_patient << endl;	
-	vector<Patient>::iterator it;
-    for (it = this->listofpatients.begin(); it != this->listofpatients.end(); ++it) {
-        it->affichernomPatient(); // Afficher les noms de DossierPatient
-    }
-}
-
-void RDV::afficherRDVMedecin(){
-	string nom_medecin;
-	cout << "	- Le rendez-vous du medecin : "<< this->nom_medecin << endl;	
-	vector<Medecin>::iterator it;
-    for (it = this->listofmedecins.begin(); it != this->listofmedecins.end(); ++it) {
-        it->affichernomMedecin(); // Afficher les noms de DossierPatient
-    }
-}
-
-void RDV::afficherRDVDate(){
-	int dateRDV;
-	cout << "	- Le rendez-vous est programmé pour le : "<< this->dateRDV << endl;	
-	vector<Date>::iterator it;
-    for (it = this->listofdates.begin(); it != this->listofdates.end(); ++it) {
-        it->display2(); // Afficher les noms de DossierPatient
-    }
-}
-
-
-// Récupérer les noms d'un objet RDV
-vector<Patient> RDV::getNomPatient(){ 
-	return this->listofpatients;
-}
-
-// Récupérer les noms d'un objet DossierPatient
-vector<Medecin> RDV::getNomMedecin(){ 
-	return this->listofmedecins;
-}
-
-// Récupérer les noms d'un objet DossierPatient
-vector<Date> RDV::getDate(){ 
-	return this->dateRDV;
-}
-
-
-// Changer les patients d'un objet DossierPatient
-void RDV::setRDVPatient(vector<Patient> listofpatients){ 
-	this->listofpatients=listofpatients;
-}
-
-// Changer l'ID d'un objet DossierPatient
-void RDV::setRDVMedecin(vector<Medecin> listofmedecins){ 
-	this->listofmedecins=listofmedecins;
-}
-
-// Changer l'ID d'un objet DossierPatient
-void RDV::setRDVDate(vector<Date> dateRDV){ 
-	this->dateRDV=dateRDV;
-}
-
+RDV::RDV(Medecin medecin,list<Prescription> prescriptions,Date date){
+	RDV::setMedecin(medecin);
+	RDV::setPrescriptions(prescriptions);
+	RDV::setDateRdv(date);
+};
+		
+// Accesseurs
+void RDV::setMedecin(Medecin medecin){
+	this->medecin=medecin;
+}:
+Medecin RDV::getMedecin(){
+	return this->medecin;
+};
+void RDV::setPrescriptions(std::list<Prescription> prescriptions) '
+	this->prescriptions=prescriptions;
+};
+list<Prescription> RDV::getPrescriptions(){
+	return this->prescriptions;
+};
+void RDV::setDateRdv(Date date){
+	this->date=date;
+};
+Date RDV::getDateRdv(){
+	this->date
+};
+	
+// Affichage
+void RDV::afficherBilanRdv(){
+	cout << "le RDV avec le docteur " << RDV::getMedecin().getnomMedecin() << " a donné lieu à " 
+		<< RDV::getPrescriptions().size() << end1
+	cout << " le RDV à eu lieu le " << end1
+	getDateRdv().affichage2();	
+};
